d391f4376c1635a4a67e63f4d30cd5e20836e816
NinhLan <82390818+NinhLan@users.noreply.github.com>
2022-05-23 03:28:10 +0200

    Update serveur.py

--- Projet_docker/serveur/serveur.py
+++ Projet_docker/serveur/serveur.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-# Import of python system libraries.
-# These libraries will be used to create the web server.
-# You don't have to install anything special, these libraries are installed with Python.
+# Importation des bibliothèques système python.
+# Ces bibliothèques seront utilisées pour créer le serveur web.
 import http.server
 import socketserver
 
-# This variable is going to handle the requests of our client on the server.
+# Cette variable va gérer les requêtes de notre client sur le serveur.
 handler = http.server.SimpleHTTPRequestHandler
 
-# Here we define that we want to start the server on port 1234. 
-# Try to remember this information it will be very useful to us later with docker-compose.
-with socketserver.TCPServer(("", 1234), handler) as httpd:
-    # This instruction will keep the server running, waiting for requests from the client.
-    httpd.serve_forever()
+# Ici nous définissons que nous voulons démarrer le serveur sur le port 1234.
+# Essayez de vous souvenir de cette information, elle nous sera très utile plus tard avec docker-compose.
+with socketserver.TCPServer(("", 1234), handler) as httpd :
+  # Cette instruction va maintenir le serveur en fonctionnement, en attendant les requêtes du client.
+  httpd.serve_forever()
